f243fc388acb04c527b06d7e9187cc3fe3549d82
IsmailM <ismail.moghul@gmail.com>
2014-11-19 11:03:37 +0000

    initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,35 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'src/<%= pkg.name %>.js',
+        dest: 'build/<%= pkg.name %>.min.js'
+      }
+    },
+    jshint: {
+      files: ['Gruntfile.js', 'src/**/*.js'],
+      options: {
+        globals: {
+          jQuery: true,
+          console: true,
+          module: true,
+          document: true
+        }
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  // Default task(s).
+  grunt.registerTask('default', ['jshint', 'uglify']);
+
+};
---
+++ src/GeneValidatorApp-API.js
@@ -0,0 +1,68 @@
+// GeneValidatorApp-API
+//
+// This function simply runs a POST request at the provided GeneValidatorApp URL.
+// You are required to provide the function the following variables:
+//
+// * Compulsory Variables 
+// *** GeneValidatorApp_url = This the url link to the GeneValidatorApp
+//      (e.g. 'genevalidator.sbcs.qmul.ac.uk')
+// *** sequence = This is the sequence(s) that are to be analysed by GeneValidator.
+//      Sequences should be in a single line with no new line characters within the
+//      sequence ('\n'). If an sequence id is present, the id should start with a 
+//      forward arrow character ('>') and there must be a new line character between
+//      the id and sequence. Muliple Sequences are allowed - there should a new line
+//      character between sequences ('\n').
+// *** database = This is the name of the chosen database that you wish GeneValidator
+//      to use. Possible options can be seen under 'Advanced Parameters' on the GeneValidatorApp.
+//
+// * Optional Variables
+// *** validations (default = all validations) = This is an string of all the validations that
+//      you want to run. Each validation must be separated by a comma. Options include: 
+//      "length_cluster, length_rank, duplication, gene_merge, multiple_alignment,
+//      blast_reading_frame, open_reading_frame"
+function send_to_genevalidator(GeneValidatorApp_url, sequence, database, validations) {
+  var GVAppUrl = '';
+  if (GeneValidatorApp_url.slice(-1) === '/') {
+    GVAppUrl = GeneValidatorApp_url +'input';
+  } else {
+    GVAppUrl = GeneValidatorApp_url +'/input';
+  }
+  var seq  = sequence.replace('\n', '%0D%0A').replace('>', '%3E');
+  var vals = sort_out_validations(validations);
+  var db   = database;
+  var data = "seq=" + seq + "&" + vals + "database=" + db + "&result_link=yes";
+  var link = '';
+  $.ajax({
+    type: 'POST',
+    url: GVAppUrl,
+    data: data,
+    async: false,
+    success: function(result){
+      link = result;
+    }
+  });
+  return link;
+}
+
+// This is an internal method that converts returns the validations variable in the required format.
+function sort_out_validations(validations){
+  if (validations === 'all'){
+    vals = 'validations%5B%5D=lenc&validations%5B%5D=lenr&validations%5B%5D=dup&validations%5B%5D=merge&validations%5B%5D=align&validations%5B%5D=frame&validations%5B%5D=orf&';
+  } else {
+    validations = validations.split(',');
+    validations_short_names = {
+      'length_cluster': 'lenc',
+      'length_rank': 'lenr',
+      'duplication': 'dup',
+      'gene_merge': 'merge',
+      'multiple_alignment': 'align',
+      'blast_reading_frame': 'frame',
+      'open_reading_frame': 'orf'
+    };
+    vals = '';
+    for (i=0; i<validations.length; i++){
+      vals += 'validations%5B%5D=' + validations_short_names[validations[i].trim()] + '&';
+    }
+  }
+  return vals;
+}
