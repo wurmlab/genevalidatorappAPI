e578b5840a7f203fe53a42a3cbf5f5e8c6f41dc5
IsmailM <ismail.moghul@gmail.com>
2015-10-07 09:17:59 +0100

    update Demo and JS

--- src/genevalidatorappAPI.js
+++ src/genevalidatorappAPI.js
@@ -30,36 +30,30 @@ if (!GV) {
   //      you want to run. Each validation must be separated by a comma. Options include:
   //      "length_cluster, length_rank, duplication, gene_merge, multiple_alignment,
   //      blast_reading_frame, open_reading_frame"
-  GV.sendToGeneValidator = function(genevalidatorAppUrl, sequence, db, validations, result_type) {
+  GV.sendToGeneValidator = function(genevalidatorAppUrl, sequence, db, validations, outputType) {
     var gvResults = ''
-    var data = GV.setUpData(sequence, validations, db, result_type)
+    var data = GV.setUpData(sequence, validations, db, outputType)
     var promise = $.ajax({
       type: 'POST',
       url: genevalidatorAppUrl,
       data: data,
     });
-    if ((result_type == 'open_link') || result_type == undefined) {
-      var loading_url = genevalidatorAppUrl + '/GeneValidator/loading.html'
-      var results_page = window.open(loading_url, '_blank');
+    if ((outputType == 'open_link') || outputType == undefined) {
+      var loadingUrl = genevalidatorAppUrl + '/GeneValidator/loading.html'
+      var resultsPage = window.open(loadingUrl, '_blank');
       promise.done(function(result) {
-        console.log(result);
-        results_page.location.href = result;
-        gvResults = result
-      })
-    } else if ( (result_type == 'results_url') || (result_type == 'json_url') ) {
-      promise.done(function(result) {
-        console.log(result);
+        resultsPage.location.href = result;
         gvResults = result
       })
     }
-    return gvResults
+    return promise
   }
 
-  GV.setUpData = function(sequence, validations, db, result_type) {
+  GV.setUpData = function(sequence, validations, db, outputType) {
     var seq  = sequence.replace('\r\n', '%0D%0A').replace('\n', '%0D%0A').replace('>', '%3E');
     var vals = GV.sortOutValidations(validations);
-    var output_type = GV.type_of_output(result_type)
-    var data = 'seq=' + seq + '&' + vals + 'database=' + db + output_type
+    var outputType = GV.typeOfOutput(outputType)
+    var data = 'seq=' + seq + '&' + vals + 'database=' + db + outputType
     return data
   }
 
@@ -70,7 +64,7 @@ if (!GV) {
       vals = 'validations%5B%5D=lenc&validations%5B%5D=lenr&validations%5B%5D=dup&validations%5B%5D=merge&validations%5B%5D=align&validations%5B%5D=frame&validations%5B%5D=orf&';
     } else {
       validations = validations.split(',');
-      var validations_short_names = {
+      var validationsShortNames = {
         'length_cluster': 'lenc',
         'length_rank': 'lenr',
         'duplication': 'dup',
@@ -81,19 +75,19 @@ if (!GV) {
       };
       vals = '';
       for (var i = 0; i<validations.length; i++){
-        vals += 'validations%5B%5D=' + validations_short_names[validations[i].trim()] + '&';
+        vals += 'validations%5B%5D=' + validationsShortNames[validations[i].trim()] + '&';
       }
     }
     return vals;
   }
 
-  GV.type_of_output = function(result_type) {
-    var output_type = ''
-    if ((result_type === 'open_link') || (result_type === 'results_url') || result_type === undefined) {
-      output_type = '&results_url=yes';
-    } else if (result_type === 'json_url') {
-      output_type = '&json_url=yes';
+  GV.typeOfOutput = function(outputType) {
+    var type = ''
+    if (outputType === 'json_url') {
+      type = '&json_url=yes';
+    } else {
+      type = '&results_url=yes';
     };
-    return output_type;
+    return type;
   }
 }());
